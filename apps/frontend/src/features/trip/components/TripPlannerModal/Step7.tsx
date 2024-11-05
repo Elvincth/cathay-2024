f563bf64b52cfc8b4f00f7e493509a4e7894de15
Elvin Chu <elvincth@gmail.com>
2024-11-05 17:01:59 +0800

    add swipe

--- apps/frontend/src/features/trip/components/TripPlannerModal/Step7.tsx
+++ apps/frontend/src/features/trip/components/TripPlannerModal/Step7.tsx
@@ -1,6 +1,7 @@
 import { IonButton } from "@ionic/react";
 import TripHeader from "./TripHeader";
 import { useEffect } from "react";
+import TinderCard from "react-tinder-card";
 
 export interface Step7Props {
   onNextStep: () => void;
@@ -8,27 +9,23 @@ export interface Step7Props {
 }
 
 export function Step7({ onNextStep, onPrevStep }: Step7Props) {
-  useEffect(() => {
-    setTimeout(() => {}, 3000);
-  }, []);
+  const images = ["/images/tinder.png", "/images/247.png", "/images/248.png"];
+
   return (
     <>
       <div>
         <TripHeader />
         <div className="p-4">
-          <div className="flex flex-nowrap overflow-x-auto">
-            <img
-              src="/images/tinder.png"
-              className="mx-2 w-[320px] flex-shrink-0"
-            />
-            <img
-              src="/images/247.png"
-              className="mx-2 w-[320px] flex-shrink-0"
-            />
-            <img
-              src="/images/248.png"
-              className="mx-2 w-[320px] flex-shrink-0"
-            />
+          <div className="relative h-[530px] w-full">
+            {images.map((image, index) => (
+              <TinderCard key={index} className="absolute inset-0">
+                <img
+                  src={image}
+                  className="h-full w-full object-cover"
+                  style={{ objectFit: "cover" }}
+                />
+              </TinderCard>
+            ))}
           </div>
           <div className="flex">
             <IonButton className="mt-2 w-full" onClick={() => onPrevStep()}>
